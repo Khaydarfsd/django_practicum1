e8fcb20f254f16ac1252fe8d91ed9305a6e79ad3
khaydarfsd <haydartursunov05@gmail.com>
2023-03-30 05:53:39 +0500

    users, password, login, register

--- news_app/views.py
+++ news_app/views.py
@@ -1,12 +1,20 @@
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.models import User
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import TemplateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from hitcount.utils import get_hitcount_model
 
 from .models import News, Category
-from .forms import ContactForm
+from .forms import ContactForm, CommentForm
+from news_project.custom_permissions import OnlyLoggedSuperUser
+
+
 
 def news_list(request):
-    # news_list = News.objects.filter(status=News.Status.Published)
     news_list = News.published.all()
 
     context = {
@@ -14,10 +22,52 @@ def news_list(request):
     }
     return render(request, 'news/news_list.html', context)
 
+
+from hitcount.views import HitCountDetailView, HitCountMixin
+
+
+#
+# class PostCountHitDetailView(HitCountDetailView):
+#     model = News
+#     count_hit = True
+
+
 def news_detail(request, news):
     news = get_object_or_404(News, slug=news, status=News.Status.Published)
+    context = {}
+    #hitcoun logic
+    hit_count = get_hitcount_model().objects.get_for_object(news)
+    hits = hit_count.hits
+    hit_context = context['hitcount'] = {'pk': hit_count.pk}
+    hitcount_response = HitCountMixin.hit_count(request, hit_count)
+    if hitcount_response.hit_counted:
+        hits = hits + 1
+        hit_context['hit_counted'] = hitcount_response.hit_counted
+        hit_context['hit_message'] = hitcount_response.hit_message
+        hit_context['total_hits'] = hits
+
+
+    comments = news.comments.filter(active=True)
+    comment_count = comments.count()
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            #yangi comment obyektini yaratamiz ln DB ga saqlamaymiz
+            new_comment = comment_form.save(commit=False)
+            new_comment.news = news
+            new_comment.user = request.user
+            # MB ga saqlaymiz
+            new_comment.save()
+            comment_form = CommentForm()
+    else:
+        comment_form = CommentForm()
     context = {
-        "news": news
+        "news": news,
+        'comments': comments,
+        'comment_count': comment_count,
+        'new_comment': new_comment,
+        'comment_form': comment_form
     }
     return render(request, 'news/news_detail.html', context)
 
@@ -107,4 +157,43 @@ class SportNewsView(ListView):
     def get_queryset(self):
         news = self.model.published.all().filter(category__name='Sport')
         return news
+
+class NewsUpdateView(OnlyLoggedSuperUser, UpdateView):
+    model = News
+    fields = ('title', 'body', 'image', 'category', 'status')
+    template_name = 'crud/news_edit.html'
+
+class NewsDeleteView(OnlyLoggedSuperUser, DeleteView):
+    model = News
+    template_name = 'crud/news_delete.html'
+    success_url = reverse_lazy('home_page')
+
+class NewsCreateView(OnlyLoggedSuperUser, CreateView):
+    model = News
+    template_name = 'crud/news_create.html'
+    fields = ('title', 'slug', 'image', 'body', 'category', 'status')
+
+@login_required
+@user_passes_test(lambda u:u.is_superuser)
+def admin_page_view(request):
+    admin_users = User.objects.filter(is_superuser=True)
+    context = {
+        'admin_users': admin_users
+    }
+    return render(request, 'pages/admin_page.html', context)
+
+
+class SearchResultsList(ListView):
+    model = News
+    template_name = 'news/search_result.html'
+    context_object_name = 'barcha_yangiliklar'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return News.objects.filter(
+            Q(title__icontains=query) | Q(body__icontains=query)
+        )
+
+
+
 
--- news_project/settings.py
+++ news_project/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news_app',
+    'hitcount',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home_page'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+LOGIN_URL = 'login'
